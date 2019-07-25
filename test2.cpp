be08c72d7e1adf3066397a0e57dc31d54a7a7fa0
fuckyou1907023 <53184980+fuckyou1907023@users.noreply.github.com>
2019-07-25 10:55:24 +0700

    bài lần 2

---
+++ test2.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+	float a,b;
+	printf("nhap so a=\n");
+	scanf("%f",&a);
+
+    printf("nhap so b=\n",b);
+    scanf(%f,&b);
+	if(b!=0){
+		if(a>b)  {
+		printf("thuong 2 so la %f", a/b );
+	}else{
+		printf("tich 2 so la %f", a*b );
+	}
+    }
+    else
+    {
+        printf(khong thoa man dieu kien cua b , moi nhap lai");
+     {
+	return 0;
+}
+
+     //hoang trong ha 
