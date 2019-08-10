7582cf8cc1fc0f8f7bcb29d7098375576a464bb6
fuckyou1907023 <53184980+fuckyou1907023@users.noreply.github.com>
2019-08-10 13:57:11 +0700

    assignment9.cpp

---
+++ chuoi1chuachuoi2.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<string.h>
+	int	main(){
+	char s1[9]="12345",s2[9] = "123";
+	printf("%s\n", strstr(s1 ,s2));
+
+	if( strstr(s1,s2) == NULL){
+		printf("khong la con \n");
+		return 0;
+	}
+		printf(" la con \n");
+		return 0;
+
+// 	printf("nhap chuoi s1: \n");
+// 	scanf("%s",s1);
+// 	printf("nhap chuoi s2: \n");
+// 	scanf("%s",s2);
+// 		for(int i=0 ; i<strlen(s1) ; ++i){
+// 			for (int j = 0; i < strlen(s2); ++j)
+// 			{
+
+
+// 				j++;
+				
+
+
+		
+	
+	
+// 			printf("chuoi thu 2 nam trong chuoi thu nhat \n");
+// 		}else{
+// 			printf("chuoi thu 2 k nam trong chuoi thu nhat \n");
+// 		}
+// 	}
+// }
+
+	}
